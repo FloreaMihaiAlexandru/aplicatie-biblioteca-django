087d541ed6f7ddc34c7006593ef95aacc2cb1415
Florea Mihai <mohaimihai22@gmail.com>
2025-08-05 21:03:22 +0300

    Login, Register (TODO admin confirm)

--- mainview/views.py
+++ mainview/views.py
@@ -1,5 +1,49 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+
+from .forms import SignUpForm
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Hello World!")
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request=request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, f'Welcome {user.username}!')
+            return redirect('home')
+        else:
+            messages.error(request, 'Invalid username or password.')
+            return redirect('home')
+    else:
+        return render(request, 'home.html',{})
+    
+
+def logout_view(request):
+    logout(request)
+    messages.success(request, 'You have been logged out.')
+    return redirect('home')
+
+
+def register_view(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+
+            #Auth and log in (to be removed later)
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, f'Account created for {username}!')
+            return redirect('home')
+        else:
+            messages.error(request, 'Error creating account. Please correct the errors below.')
+            return render(request, 'register.html', {'form': form})
+    else:
+        form = SignUpForm()
+        return render(request, 'register.html', {'form': form})
